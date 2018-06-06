f962dd4b3e02c9aeee7d05b2430803137295ca85
ohduran <alvaro.duranb@gmail.com>
2018-06-06 17:12:31 +0100

    Added download_delay class attribute

--- fundrazr/fundrazr/spiders/fundrazr_scrape.py
+++ fundrazr/fundrazr/spiders/fundrazr_scrape.py
@@ -11,6 +11,7 @@ class Fundrazr(scrapy.Spider):
     start_urls = ["https://fundrazr.com/find?category=Health"]
 
     npages = 2
+    download_delay = 2
 
     # This mimics getting the pages using the next button
     for i in range(2, npages + 2):
