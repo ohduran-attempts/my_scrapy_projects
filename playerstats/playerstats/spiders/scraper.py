68289b6379566e386e87dc38a754927ef50c7ff9
ohduran <alvaro.duranb@gmail.com>
2018-06-08 08:06:33 +0100

    Player name and Player position

--- playerstats/playerstats/spiders/scraper.py
+++ playerstats/playerstats/spiders/scraper.py
@@ -9,21 +9,19 @@ class PlayerStastsScraper(scrapy.Spider):
 
     def parse(self, response):
         """Parse the web start_urls."""
+        items = {}
         # Since we are looking for a class, we will use '.set' in the selector.
-        POSITION_SELECTOR = '.posiciones-equipo .posiciones'
+        POSITION_SELECTOR = '.posiciones-equipo'
 
-        for position in response.css(POSITION_SELECTOR):
-            NAME_SELECTOR = '.nombre-perfil ::text'
-            yield {
-                'player_name': position.css(NAME_SELECTOR).extract_first(),
-            }
+        for position in response.css(POSITION_SELECTOR)[1:]:
+            POSITION_NAME_SELECTOR = '.titulo_posicion ::text'
+            PLAYER_NAME_SELECTOR = ' .posiciones .nombre-perfil ::text'
 
+            items['position'] = position.css(
+                POSITION_NAME_SELECTOR).extract_first()
+            items['player_name'] = position.css(
+                PLAYER_NAME_SELECTOR).extract_first()
 
-#/html/body/div[2]/section[2]/div/section/div[2]/div[2]/div[2]/a[1]/div[2]
-        # NEXT_PAGE_SELECTOR = '.next a ::attr(href)'
-        # next_page = response.css(NEXT_PAGE_SELECTOR).extract_first()
-        # if next_page:
-        #     yield scrapy.Request(
-        #         response.urljoin(next_page),
-        #         callback=self.parse  # recursively call self parse until next_page is None
-        #     )
+            # REMOVE NONE VALUES
+
+            yield items
