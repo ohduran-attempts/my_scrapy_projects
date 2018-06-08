a22e1c5264c6f2b5046dc11c9db0685b808368b5
ohduran <alvaro.duranb@gmail.com>
2018-06-08 08:45:57 +0100

    Player names and positions implemented

--- playerstats/playerstats/spiders/scraper.py
+++ playerstats/playerstats/spiders/scraper.py
@@ -9,24 +9,20 @@ class PlayerStastsScraper(scrapy.Spider):
 
     def parse(self, response):
         """Parse the web start_urls."""
-        items = {}
-        # Since we are looking for a class, we will use '.set' in the selector.
+
         POSITION_SELECTOR = '.posiciones-equipo'
-
         for position in response.css(POSITION_SELECTOR)[1:]:
-            POSITION_NAME_SELECTOR = '.titulo_posicion ::text'
+            player_items = {}
 
-            items['position'] = position.css(
+            POSITION_NAME_SELECTOR = '.titulo_posicion ::text'
+            player_items['position'] = position.css(
                 POSITION_NAME_SELECTOR).extract_first()
 
             PLAYER_SELECTOR = '.posiciones .box-jugador'
-
             for player in position.css(PLAYER_SELECTOR):
+
                 PLAYER_NAME_SELECTOR = '.nombre-perfil ::text'
-                items['player_name'] = position.css(
+                player_items['player_name'] = player.css(
                     PLAYER_NAME_SELECTOR).extract_first()
 
-                # REMOVE NONE VALUES
-                yield items
-
-            # REMOVE NONE VALUES
+                yield player_items
