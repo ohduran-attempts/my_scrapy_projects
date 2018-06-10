e949131a9d406ff681dadd3978716ab52f87089d
ohduran <alvaro.duranb@gmail.com>
2018-06-10 08:38:59 +0100

    More changes on scraper to nest player_stats under player_items

--- playerstats/playerstats/spiders/scraper.py
+++ playerstats/playerstats/spiders/scraper.py
@@ -1,5 +1,5 @@
 import scrapy
-from playerstats.items import PlayerItem
+from playerstats.items import PlayerItem, StatsItem
 
 
 class PlayerStastsScraper(scrapy.Spider):
@@ -13,54 +13,57 @@ class PlayerStastsScraper(scrapy.Spider):
         player_items = PlayerItem()
 
         POSITION_SELECTOR = '.posiciones-equipo'
+        POSITION_NAME_SELECTOR = '.titulo_posicion ::text'
+        PLAYER_SELECTOR = '.posiciones .box-jugador'
+        PLAYER_NAME_SELECTOR = '.nombre-perfil ::text'
+        STATS_PAGE_SELECTOR = '::attr(href)'
+
         for position in response.css(POSITION_SELECTOR)[1:]:
 
-            POSITION_NAME_SELECTOR = '.titulo_posicion ::text'
             player_items['position'] = position.css(
                 POSITION_NAME_SELECTOR).extract_first()
 
-            PLAYER_SELECTOR = '.posiciones .box-jugador'
             for player in position.css(PLAYER_SELECTOR):
 
-                PLAYER_NAME_SELECTOR = '.nombre-perfil ::text'
                 player_items['name'] = player.css(
                     PLAYER_NAME_SELECTOR).extract_first()
 
-                # STATS_PAGE_SELECTOR = '::attr(href)'
-                # stats_page = player.css(STATS_PAGE_SELECTOR).extract_first()
-                # if stats_page:
-                #     stats = scrapy.Request(
-                #         stats_page,
-                #         callback=self.parse_stats)
-                #
-                # player_items['stats'] = stats
+                stats_page = player.css(STATS_PAGE_SELECTOR).extract_first()
+                if stats_page:
+                    player_stats = StatsItem()
+                    stats = scrapy.Request(
+                        stats_page,
+                        callback=self.parse_stats)
+                    stats.meta['stats'] = player_stats
+
+                player_items['stats'] = stats
                 yield player_items
 
     def parse_stats(self, response):
         """Parse specific stats page for each player"""
         # http://laliga.es/en/player/keylor-navas
-        player_stats = {}
+        player_stats = response.meta['stats']
         stats_keys = [
-            'min',
-            'games played',
-            'PC games played',
-            'full games played',
-            'PC full games played',
-            'games started',
-            'PC games started',
-            'games where substituted',
-            'PC games where substituted',
-            'yellow cards',
-            'red cards',
-            '2 yellow cards',
-            'goals scored',
-            'penalties scored',
-            'own goals',
-            'goals conceded',
+            'minutes',
+            'games_played',
+            'games_played_pc',
+            'games_played_full',
+            'games_played_full_pc',
+            'games_started',
+            'games_started_pc',
+            'games_where_substituted',
+            'games_where_subsituted_pc',
+            'cards_yellow',
+            'cards_red',
+            'cards_doubleyellow',
+            'goals_scored',
+            'goals_scored_penalty',
+            'goals_owngoals',
+            'goals_conceded',
         ]
 
         a_selector_css = '.estadisticas-jugador-1'
-        b_selector_xpath_0 = '//section[@id="box-estadisticas-jugador"]'  #take element 0 of this
+        b_selector_xpath_0 = '//section[@id="box-estadisticas-jugador"]'  # take element 0 of this
         c_selector_css_1 = 'div'  # take element 1 of this
         d_selector_xpath = '//table[@class="datatable"]/tbody/tr'  # for item in d
         e_selector_xpath = 'td/text()'
@@ -72,11 +75,11 @@ class PlayerStastsScraper(scrapy.Spider):
 
         stats_values_by_condition = []
         for item in d:
-            e = item.xpath('td/text()').extract()
+            e = item.xpath(e_selector_xpath).extract()
             stats_values_by_condition.append(e)
 
         for condition in stats_values_by_condition:
                     player_stats_in_condition = dict(zip(stats_keys, condition[1:]))
                     player_stats[condition[0]] = player_stats_in_condition
 
-        return player_stats
+        yield player_stats
